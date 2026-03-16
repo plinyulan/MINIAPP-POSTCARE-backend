0f0393ee129041ab24e1022e9a6cff4d8a9bfae8
plinyulan <patty.linyulan@gmail.com>
2026-03-16 16:08:22 +0700

    rename db

--- database.js
+++ database.js
@@ -4,10 +4,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  host: process.env.INSTANCE_CONNECTION_NAME
-    ? `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
-    : process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
+  host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
 });
 
 module.exports = pool;
