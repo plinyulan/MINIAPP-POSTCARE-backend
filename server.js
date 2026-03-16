b10b29360b642557c70b56e54c027530529d67dc
plinyulan <patty.linyulan@gmail.com>
2026-03-16 16:09:40 +0700

    see error

--- server.js
+++ server.js
@@ -13,13 +13,20 @@ app.get("/", (req, res) => {
 
 app.get("/test-db", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM appointments");
-    res.json(result.rows);
+    const result = await pool.query("SELECT NOW()");
+    res.json({ success: true, data: result.rows[0] });
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
+    console.error("DB ERROR:", err);
+    res.status(500).json({
+      message: err.message,
+      code: err.code,
+      errno: err.errno,
+      syscall: err.syscall,
+      address: err.address
+    });
   }
 });
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
