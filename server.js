d3b28dab1c9f131721146e5e3b102d88f64d6518
plinyulan <patty.linyulan@gmail.com>
2026-03-20 21:48:30 +0700

    logic test home

--- server.js
+++ server.js
@@ -23,7 +23,12 @@ app.get("/test-db", async (req, res) => {
 
 app.get("/appointments", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM appointments ORDER BY id DESC");
+    const result = await pool.query(`
+      SELECT *
+      FROM appointments
+      ORDER BY appointment_date ASC, time_slot ASC
+    `);
+
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
